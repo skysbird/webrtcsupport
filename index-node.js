0669f576582c53a3a42aa5ac014fcc5966809615
David Dias <daviddias.p@gmail.com>
2017-05-19 20:13:26 +0100

    fix: linting -> move to standardjs

--- index-node.js
+++ index-node.js
@@ -2,19 +2,19 @@
 // be easily added to dependencies, we'll update this to expose it
 
 module.exports = {
-    support: false,
-    supportRTCPeerConnection: false,
-    supportVp8: false,
-    supportGetUserMedia: false,
-    supportDataChannel: false,
-    supportWebAudio: false,
-    supportMediaStream: false,
-    supportScreenSharing: false,
-    prefix: undefined,
-    AudioContext: undefined,
-    PeerConnection: undefined,
-    SessionDescription: undefined,
-    IceCandidate: undefined,
-    MediaStream: undefined,
-    getUserMedia: undefined
-};
+  support: false,
+  supportRTCPeerConnection: false,
+  supportVp8: false,
+  supportGetUserMedia: false,
+  supportDataChannel: false,
+  supportWebAudio: false,
+  supportMediaStream: false,
+  supportScreenSharing: false,
+  prefix: undefined,
+  AudioContext: undefined,
+  PeerConnection: undefined,
+  SessionDescription: undefined,
+  IceCandidate: undefined,
+  MediaStream: undefined,
+  getUserMedia: undefined
+}
